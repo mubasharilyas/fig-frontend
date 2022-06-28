3821ce3e83bb5e813c1cbdf552d15d9f2a1e9b0b
mubasharilyas <mubasharbhatti485@gmail.com>
2022-06-28 16:45:40 +0500

    api service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  search: String = ''
+  tags = [];
+  individauls = []
+  constructor(private api: ApiService) { }
+  ngOnInit(): void {
+    this.getTags()
+  }
+  getTags() {
+    this.api.get('https://personal-fig:klb655IIBPUJUCFNBVgRbRZUrim8oTzV@api.swell.store/categories').subscribe((res: any) => {
+      this.tags = res.results
+    })
+  }
+  searchBytag() {
+    this.api.get('https://personal-fig:klb655IIBPUJUCFNBVgRbRZUrim8oTzV@api.swell.store/products?category=' + this.search).subscribe((res: any) => {
+      this.individauls = res.results
+    })
 
-  constructor() {}
+  }
+
 
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  headers = new HttpHeaders()
+    .append('Content-Type', 'application/json')
+    .append('Access-Control-Allow-Headers', 'Content-Type')
+    .append('Access-Control-Allow-Methods', 'GET')
+    .append('Access-Control-Allow-Origin', '*');
+  constructor(private http: HttpClient) { }
+  get(url: any) {
+    return this.http.get(url)
+  }
+}
