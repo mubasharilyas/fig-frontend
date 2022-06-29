ade0c6dbbb1f76691e39c00ac10243a6044d3af6
mubasharilyas <mubasharbhatti485@gmail.com>
2022-06-29 17:17:58 +0500

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + 'www'));
 app.get('/*', function(req,res)
-{res.sendFile(path.join(__dirname+'/dist/<appname>/index.html'));});
+{res.sendFile(path.join(__dirname+'www/index.html'));});
 app.listen(process.env.PORT || 8080);
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,8 +18,11 @@ export class HomePage implements OnInit {
     this.getTags()
   }
   getTags() {
+    this.isLoading=true
     this.api.get('https://cdn.builder.codes/api/v1/proxy-api?url=https%3A%2F%2Fpersonal-fig%3Aklb655IIBPUJUCFNBVgRbRZUrim8oTzV%40api.swell.store%2Fcategories').subscribe((res: any) => {
       this.tags = res.results
+      this.isLoading=false
+
     })
   }
 
@@ -38,7 +41,14 @@ export class HomePage implements OnInit {
     }
     this.isLoading = true
     this.api.get('https://cdn.builder.codes/api/v1/proxy-api?url=https%3A%2F%2Fpersonal-fig%3Aklb655IIBPUJUCFNBVgRbRZUrim8oTzV%40api.swell.store%2Fproducts?category=' + this.selectedCategory).subscribe((res: any) => {
-      this.individauls = res.results
+      this.individauls = res.results.map(ind=>{
+        ind.bio=`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries`;
+        ind.reviews=`Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries`;
+        ind.isBioExpended=false;
+        ind.isReviewExpended=false
+        return ind;
+      })
       this.isLoading = false
 
     })
